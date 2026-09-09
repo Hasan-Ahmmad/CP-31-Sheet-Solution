bfc4446973ff8edeaa536cd49d32f393aac7c5a8
Hasan Ahmmad <mdhasanahmmad335@gmail.com>
2026-09-09 20:22:17 +0600

    Create 25. Bad Boy.cpp

---
+++ 900/25. Bad Boy.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define ll long long
+
+int main(){
+    
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int t; cin>>t;
+    while(t--){
+    
+        int n, m, x,y;
+        cin>>n>>m>>x>>y;
+
+        cout<< 1 <<" "<< m <<" "<< n <<" "<< 1 <<endl;
+    
+    }
+    return 0;
+}
